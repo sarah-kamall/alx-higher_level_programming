0b494d06761ada2419e34e99c0fbb5286286b497
sarah-kamall <sarahkamal1712@gmail.com>
2023-10-04 01:11:21 +0300

    py 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            print(chr(ord(str[i]) - 32), end="")
+        else:
+            print(str[i], end="")
+    print()
