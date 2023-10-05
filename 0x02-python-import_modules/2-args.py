43052d6daf3b5bf2e524eab07d7d6ace9573a3c6
sarah-kamall <sarahkamal1712@gmail.com>
2023-10-05 16:08:23 +0300

    task 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import sys
-numargs = len(sys.argv) - 1
-if numargs > 0:
-    print("{} arguments:".format(numargs))
-else:
-    print("{} arguments.".format(numargs))
-for i in range(1, numargs + 1):
-    print("{} : {}".format(i, sys.argv[i]))
+def main():
+    numargs = len(sys.argv) - 1
+    if numargs > 0:
+        print("{} arguments:".format(numargs))
+    else:
+        print("{} arguments.".format(numargs))
+    for i in range(1, numargs + 1):
+        print("{} : {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    main()
