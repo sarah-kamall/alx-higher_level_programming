70a73ef3ff8e98db50093c16d22fa2d6898329e3
sarah-kamall <sarahkamal1712@gmail.com>
2023-10-05 15:07:09 +0300

    task 0 adj

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-sum = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+from add_0 import add
+def main():
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
+if __name__ == "__main__":
+    main()
