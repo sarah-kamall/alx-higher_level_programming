49d9e7bcd873e31927a841a417fd1632860850ec
sarah-kamall <sarahkamal1712@gmail.com>
2023-10-05 15:15:18 +0300

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,13 +3,9 @@ import calculator_1 as c1
 def main():
     a = 10
     b = 5
-    sum = c1.add(a, b)
-    sub = c1.sub(a, b)
-    mul = c1.mul(a, b)
-    div = c1.div(a, b)
-    print("{} + {} = {}".format(a, b, sum))
-    print("{} + {} = {}".format(a, b, sub)) 
-    print("{} + {} = {}".format(a, b, mul)) 
-    print("{} + {} = {}".format(a, b, div)) 
+    print("{} + {} = {}".format(a, b, c1.add(a, b)))
+    print("{} + {} = {}".format(a, b, c1.sub(a, b))) 
+    print("{} + {} = {}".format(a, b, c1.mul(a, b))) 
+    print("{} + {} = {}".format(a, b, c1.div(a, b))) 
 if __name__ == "__main__":
     main()
