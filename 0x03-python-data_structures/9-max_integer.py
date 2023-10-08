ff9ffdd54a23b4a776ba4fba8c424ff0712c09f7
sarah-kamall <sarahkamal1712@gmail.com>
2023-10-08 22:04:04 +0300

    tsk 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maxi = my_list[0]
+    for num in my_list:
+        if num > maxi:
+            maxi = num
+    return maxi
