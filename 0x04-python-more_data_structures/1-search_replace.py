cbc4282e374d0f7a5b570297df4db2cf5c183914
sarah-kamall <143711089+sarah-kamall@users.noreply.github.com>
2023-10-11 13:16:11 +0300

    Create 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    newlist=[]
+    for i in my_list:
+        if i == search:
+            newlist.append(replace)
+        else:
+            newlist.append(i)
+    return newlist
