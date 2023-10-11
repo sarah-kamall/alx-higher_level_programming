3ad45b0b4184dd7aa0f168c5bdedbbd77cadecc7
sarah-kamall <143711089+sarah-kamall@users.noreply.github.com>
2023-10-11 13:06:59 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-     newmat = [[0 for _ in row] for row in matrix]
+    newmat = [[0 for _ in row] for row in matrix]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             newmat[i][j]=matrix[i][j] **2
