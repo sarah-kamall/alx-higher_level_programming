811b40179ce8ee1527e0618a626970cf7f22d0b7
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-01 14:27:17 +0200

    task 0,1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""file containing a class Rectangle"""
+class Rectangle:
+    """Represent a Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""file containing a class Rectangle"""
+class Rectangle:
+    """Represent a Rectangle"""
+    def checkvalueint(self, value):
+        """check if value is of type int and valid dimention"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        return True
+    def __init__(self, width = 0, height = 0):
+        """function to initilize rectangle
+        Args:
+            width
+            height
+        """
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        """get rectangle width"""
+        return self.width
+    @width.setter
+    def width(self, value):
+        """set width of rectangle"""
+        if (self.checkvalueint(value)):
+            self.__width = value
+    @property
+    def height(self):
+        """get rectangle height"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if(self.checkvalueint(value)):
+            self.__height = value
+
+    pass
+
