a47d2459d001088daddcbb44582d738583ed8247
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-06 20:52:22 +0200

    tsk 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+file that has class square
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    
+    """class square that inherits from rectangle"""
+
+    def __init__(self, size):
+        """square initializer"""
+        integervalidator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+    
+    def area(self):
+        """calculates area"""
+        return self.__size **2
+
