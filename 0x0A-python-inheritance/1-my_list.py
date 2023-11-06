4cf50c7855faa45d7b307c106e171f37575a625c
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-06 19:15:07 +0200

    task 0,1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,14 @@
+
 #!/usr/bin/python3
 """ class Mylist that inherits from list"""
 
 class Mylist(list):
-    """Implements sorted printing for the built-in list class."""
-    
+    """
+    Implements sorted printing for the built-in list class.
+    """ 
+    def __init__(self):
+        """initialize base class"""
+        super.__init__()
     def print_sorted(self):
         """sorts and prints list"""
         
