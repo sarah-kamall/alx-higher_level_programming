333a5e96740b0b78800e53dc0a4410ea285e9bac
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-06 19:30:04 +0200

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" 
+File with one func
+"""
+
+def is_same_class(obj, a_class):
+    """ function returns true if obj is same type of class"""
+    return(type(obj) == a_class)
