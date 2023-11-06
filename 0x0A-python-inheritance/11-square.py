b9a83cfe2d089b7a334312248cff58b03cf5a545
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-06 20:58:06 +0200

    tsk 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+file that has class square
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    
+    """class square that inherits from rectangle"""
+
+    def __init__(self, size):
+        """square initializer"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+    
+    def area(self):
+        """calculates area"""
+        return self.__size **2
+    
+    def __str__(self):
+        """gets informal string representation of class"""
+        string = "[Square] {:d}/{:d}".format(size, size)
