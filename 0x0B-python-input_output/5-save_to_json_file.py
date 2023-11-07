02f3646348260b81c00f0ead41fb9a67e1084b68
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-07 14:42:48 +0200

    tsk 7

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,6 +7,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """function to make a json string from obj and write to file"""
     stri = json.dumps(my_obj)
-    with open(filename, 'a') as file:
+    with open(filename, 'w') as file:
         file.write(stri)
 
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
 try:
-    load_from_json_file(filename)
+    mylist = load_from_json_file(filename)
 except FileNotFoundError:
     mylist = []
 for i in argv[1: ]:
