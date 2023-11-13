ffd420826d4374470f495d5d91cd17f36abaa95d
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-14 00:24:45 +0200

    tsk 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+from models.base import Base
+"""class rectangle"""
+
+class Rectangle(Base):
+    """Class Rectangle"""
+    """constructor"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor imp"""
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+    """setters and getters"""
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self,value):
+        if not isinstance(value, int):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        self.__width = value
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self,value):
+        if not isinstance(value, int):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        self.__height = value
+    @property
+    def x(self):
+        return self.__x;
+    @x.setter
+    def x(self,value):
+        if not isinstance(value, int):
+            raise TypeError
+        self.__x = value
+    @property 
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        self.__y = value
+
+
