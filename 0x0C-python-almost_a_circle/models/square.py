dcf66f970aea918cb1701703d4114deb963d101b
sarah-kamall <sarahkamal1712@gmail.com>
2023-11-14 02:42:33 +0200

    tsk 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -62,6 +62,12 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
-                    
+    @property
+    def size(self):
+        return self.width
 
+    @size.setter
+    def size(self,value):
+        self.width = value
+        self.height = value
 
