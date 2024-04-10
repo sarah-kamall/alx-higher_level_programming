c6a263fb182a6cce2a5e1f9173fa9fabef77c009
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-10 23:20:36 +0200

    3

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 let counter=0;
-if (process.argv[2]!=undefined)
-    counter=process.argv[2];
-let mySents="C is fun";
-for (let i=0;i<counter;i++)
+if (process.argv[2]==undefined||isNaN(process.argv[2]))
+    console.log("Missing number of occurrences");
+else
 {
-    console.log(mySents);
+    counter=process.argv[2];
+    let mySents="C is fun";
+    for (let i=0;i<counter;i++)
+    {
+        console.log(mySents);
 
+    }
 }
