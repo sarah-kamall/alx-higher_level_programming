d3ae83c299f4e41deaac634f74986938d3a6c3b4
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-10 23:39:41 +0200

    3

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
-let counter=0;
-if (process.argv[2]==undefined||isNaN(process.argv[2]))
-    console.log("Missing size");
-else
-{
-    counter=process.argv[2];
-    for (let j=0;j<counter;j++)
-    {
-       
-        
-            console.log("X".repeat(counter));
 
-        
-    }
+function add(a,b)
+{
+    const c= a+b;
+    console.log(c);
 }
+
+add(Number(process.argv[2]), Number(process.argv[3]));
