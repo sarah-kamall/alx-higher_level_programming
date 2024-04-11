d1348d2fab8c002fabc8e9f65c665ed7456b23ce
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-11 13:19:57 +0200

    2

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,9 +1,34 @@
 #!/usr/bin/node
- class Rectangle{
+class Rectangle{
     constructor(w, h)
     {
-        this.width = w;
-        this.height = h;
+        if (isNaN(w)    ||isNaN(h)  ||w<=0 ||h<=0)
+        {
+
+        }
+        else
+        {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    print() {
+        for (let i=0;i<this.height;i++)
+        {
+            console.log('X'.repeat(this.width));
+
+        }
+        
+    }
+    rotate(){
+        let tmp = this.height;
+        this.height=this.width;
+        this.width=tmp;
+    }
+    double()
+    {
+        this.height=this.height*2;
+        this.width=this.width*2;
     }
 }
 module.exports= Rectangle;
