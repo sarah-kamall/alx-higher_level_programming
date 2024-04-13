27150b17a376bebb7a151d3fc3bb20fe0e3ea4a3
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-13 20:48:24 +0200

    ll

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-
 """  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    
-    if len(sys.argv) != 4:
-        exit()
-
-    username, password, database = sys.argv[1:]
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-        )
-    try:
-        if connection:
-            myCursor=connection.cursor()
-            myCursor.execute("SELECT * FROM states ORDER BY id ASC")
-            states=myCursor.fetchall()
-            for state in states:
-                print(state)
-            myCursor.close()
-            connection.close()
-    except:
-        exit()
-        
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
