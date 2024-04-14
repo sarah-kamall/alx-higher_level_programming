3b170faf8549ea71189af5fa2010598c47bc3d5e
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-15 00:04:05 +0200

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,4 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).first()
-    print(str(state.id) + ": " + state.name)
+    if state is None:
+        print("Nothing")
+    else:
+        print(str(state.id) + ": " + state.name)
