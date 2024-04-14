04ff1b10849fc6bad439650ed0ca1040a359ac11
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-15 00:40:15 +0200

    12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,9 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.add(State(name='Louisiana'))
-    state = session.query(State).filter_by(name='Louisiana').first()
-    if state is not None:
-        print(state.id)
+    state = session.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
     session.commit()
     session.close()
