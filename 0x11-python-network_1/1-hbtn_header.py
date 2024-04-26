3fb2dfc3c800de943582629e8b8e7b4f806c98f7
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-27 01:59:08 +0300

    111

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,12 @@ import sys
 - takes in a URL,
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
+dict(response.headers).get('X-Request-Id')
+makes a shallow copy of response.headers and returns null if key isnt found
+while response.headers['key'] returns a key error !
 """
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get('X-Request-Id'))
